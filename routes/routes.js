6c98ca92a4f8619a5b1ea17ac820bb693ab05b2c
aknietmaulen <akniet.maulenova.2017@gmail.com>
2024-02-25 17:58:30 +0600

    /

--- routes/routes.js
+++ routes/routes.js
@@ -91,7 +91,11 @@ router.get('/logout', (req, res) => {
     res.redirect('/mainPage');
 });
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
+    res.redirect('/mainPage');
+});
+
+router.get('/mainPage', async (req, res) => {
     try {
         const apiKey = '8sRU2v2QZRP1KlDDaD0hm8j0sDQHQO2vrwnf8gxb';
 
